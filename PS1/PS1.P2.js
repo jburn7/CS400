fafc80838c216de50851fea776a9ff0b19317099
Jordan <jleeburnett@gmail.com>
2020-02-04 17:26:27 -0500

    changed p2 to return a function

--- PS1/PS1.P2.js
+++ PS1/PS1.P2.js
@@ -1,21 +1,22 @@
 let str1 = '4+2', str2 = '5*7', str3 = '6-1', str4 = '9/2';
 const fun2 = s => {
-	let terms = s.split('');
-	switch(terms[1]){
+	switch(s.charAt(1)){
 		case '+':
-			return parseInt(terms[0]) + parseInt(terms[2]);
+			return (expr) => parseInt(expr.substr(0, 1) + expr.substr(2, 1));
 			break;
 		case '-':
-			return terms[0] - terms[2];
+			return (expr) => {
+				return parseInt(expr.substr(0, 1)) - parseInt(expr.substr(2, 1));
+			}
 			break;
 		case '*':
-			return terms[0] * terms[2];
+			return (expr) => parseInt(expr.substr(0, 1) * expr.substr(2, 1));
 			break;
 		case '/':
-			return terms[0] / terms[2];
+			return (expr) => parseFloat(expr.substr(0, 1) / expr.substr(2, 1));
 			break;
 		default:
 			break;
 	}
 };
-console.log(`${fun2(str1)}\n${fun2(str2)}\n${fun2(str3)}\n${fun2(str4)}`);
+console.log(`${fun2(str1)(str1)}\n${fun2(str2)(str2)}\n${fun2(str3)(str3)}\n${fun2(str4)(str4)}`);
