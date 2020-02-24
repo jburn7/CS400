ecc9d16e4a6c7b9b67985dc92274df2f7e65f62c
Jordan <jleeburnett@gmail.com>
2020-02-24 13:14:43 -0500

    reformatted POST

--- PS3/routes/ps3.js
+++ PS3/routes/ps3.js
@@ -3,9 +3,9 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('ps3', { str: 'Hello, Perry' });
+	res.render('ps3', { string: 'Hello, Perry' });
 });
 
-router.post('/', (req, res, next) => res.render('ps3', {str: req.query.str, len: req.query.str.length}))
+router.post('/', (req, res, next) => res.render('ps3', {string: req.body.string, len: req.body.string.length}))
 
 module.exports = router;
