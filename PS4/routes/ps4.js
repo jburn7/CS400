1631ef2f02ff40d97acd24f4fa4ccd6a6a95aee8
Jordan <jleeburnett@gmail.com>
2020-03-30 14:03:55 -0400

    need to install request

---
+++ PS4/routes/ps4.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let request = require('request');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var options = {
+		'method': 'GET',
+		'url': 'http://philosophy-quotes-api.glitch.me/quotes/author/Rumi',
+		'headers': {
+		}
+	};
+	request(options, function (error, response) {
+		if (error) throw new Error(error);
+		let quotes = [];
+		for(let i = 0; i < request.body.length; i++)
+		{
+			console.log("new quote");
+			quotes.add(request.body[i]);
+		}
+		res.render('index', { title: 'PS4', quotes: quotes });
+	});
+});
+
+module.exports = router;
