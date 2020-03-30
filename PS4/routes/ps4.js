ccc9873cb1f170a534c2c42b638b2c1c54536586
Jordan <jleeburnett@gmail.com>
2020-03-30 14:16:10 -0400

    ps4 v1

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -13,10 +13,11 @@ router.get('/', function(req, res, next) {
 	request(options, function (error, response) {
 		if (error) throw new Error(error);
 		let quotes = [];
-		for(let i = 0; i < request.body.length; i++)
+		let json = JSON.parse(response.body);
+		for(let i = 0; i < json.length; i++)
 		{
 			console.log("new quote");
-			quotes.add(request.body[i]);
+			quotes.push(json[i].quote);
 		}
 		res.render('index', { title: 'PS4', quotes: quotes });
 	});
